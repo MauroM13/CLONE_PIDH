1f72fb871a6238d901d9eccf902e9328636762f0
Afra Taiza <afrataiza@gmail.com>
2023-10-31 22:04:34 -0300

    feat: add link

--- frontend/PIDH/src/components/Header/Logo/index.jsx
+++ frontend/PIDH/src/components/Header/Logo/index.jsx
@@ -1,11 +1,12 @@
 import logo from '../../../assets/logo.png';
+import{ Link } from 'react-router-dom';
 
 function Logo() {
   return (
-    <div className="flex items-center py-2">
+    <Link to="/" className="flex items-center gap-1">
       <img src={logo} alt="Logo" className="w-12 h-12"/>
-      <span className="text-violet-500"> Five Hotels</span>
-    </div>
+      <span className="text-green-400 text-xl"> Five Hotels</span>
+    </Link>
   )
 }
 
