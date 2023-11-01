7dec957e75b06afe3c2c4f9c076b727a27609951
Afra Taiza <afrataiza@gmail.com>
2023-10-31 22:03:45 -0300

    style: update styles

--- frontend/PIDH/src/App.jsx
+++ frontend/PIDH/src/App.jsx
@@ -1,11 +1,13 @@
+import Footer from "./components/Footer";
 import RoutesApp from "./routes";
 
 function App() {
 
   return (
-    <>
+    <main className="overflow-x-hidden bg-gray-50 dark:bg-zinc-800 dark:text-zinc-50">
       <RoutesApp />
-    </>
+      <Footer />
+    </main>
   )
 }
 
--- frontend/PIDH/src/components/Header/index.jsx
+++ frontend/PIDH/src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import Logo from "./Logo"
+import Navbar from "./Navbar"
 
 function Header() {
   return (
-    <header className="w-screen h-20">
+    <header className="w-screen h-20 flex items-center justify-between px-8 shadow-md bg-white dark:bg-zinc-900">
       <Logo />
+      <Navbar />
     </header>
   )
 }
